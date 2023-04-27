fbe5a27da8c6cd9d24846af1daa39178f8294926
Todd Valentic <todd.valentic@gmail.com>
2023-04-27 19:58:28 +0000

    Add byte totals

--- support/lib/sql/totaldays.sql
+++ support/lib/sql/totaldays.sql
@@ -19,6 +19,7 @@ with dailysummary as (
         station,
         instrument,
         count(numimages) as datadays,
+        sum(numimages) as totalimages,
         min(firsttime::date) as firstday,
         max(lasttime::date) as lastday,
         max(lasttime::date) - min(firsttime::date) + 1 as totaldays 
@@ -26,17 +27,42 @@ with dailysummary as (
         dailysummary
     group by
         station, instrument
+
+), sizesummary as (
+
+    select
+        station.name as station,
+        instrument.name as instrument,
+        sum(image.image_bytes) as totalbytes
+    from    
+        stationinstrument
+        join
+            station on station.id = stationinstrument.station_id
+        join
+            instrument on instrument.id = stationinstrument.instrument_id
+        join
+            system_model on system_model.id = station.model_id
+        join
+            image on image.stationinstrument_id = stationinstrument.id
+    where
+        system_model.name = 'mcs'
+    group by
+        station, instrument
 )
 
 select
-    station as "Station",
-    instrument as "Instrument",
+    ts.station as "Station",
+    ts.instrument as "Instrument",
     datadays as "Data Days",
     totaldays as "Total Days",
     format('%s %%', to_char(datadays::real / totaldays * 100, '999')) as "Uptime",
+    totalimages as "Total Images",
+    format('%10s', pg_size_pretty(totalbytes)) as "Total Bytes",
     to_char(firstday, 'YYYY-MM-DD') as "First Day",
     to_char(lastday, 'YYYY-MM-DD') as "Last Day"
 from 
-    totalsummary
+    totalsummary as ts
+    join
+        sizesummary as ss on ss.station = ts.station and ss.instrument = ts.instrument
 order by
-    station, instrument
+    ts.station, ts.instrument
