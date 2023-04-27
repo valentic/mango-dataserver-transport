1291a1bb3e32c7aeacfa167a4289dda81f381773
Todd Valentic <todd.valentic@gmail.com>
2023-04-27 22:36:17 +0000

    Add SQL total_summary_view

---
+++ support/lib/sql/total_summary_view.sql
@@ -0,0 +1,70 @@
+CREATE OR REPLACE VIEW total_summary AS
+
+WITH dailysummary as (
+
+    SELECT
+        station,
+        instrument,
+        count(island) as numimages,
+        min(timestamp) as firsttime,
+        max(timestamp) as lasttime
+    FROM
+        image_islands
+    GROUP BY
+        station, instrument, island
+    ORDER BY
+        station, instrument, island
+
+), totalsummary as (
+
+    SELECT
+        station,
+        instrument,
+        count(numimages) as datadays,
+        sum(numimages) as totalimages,
+        min(firsttime::date) as firstday,
+        max(lasttime::date) as lastday,
+        max(lasttime::date) - min(firsttime::date) + 1 as totaldays 
+    FROM 
+        dailysummary
+    GROUP BY
+        station, instrument
+
+), sizesummary as (
+
+    SELECT
+        station.name as station,
+        instrument.name as instrument,
+        sum(image.image_bytes) as totalbytes
+    FROM    
+        stationinstrument
+        JOIN
+            station on station.id = stationinstrument.station_id
+        JOIN
+            instrument on instrument.id = stationinstrument.instrument_id
+        JOIN
+            system_model on system_model.id = station.model_id
+        JOIN
+            image on image.stationinstrument_id = stationinstrument.id
+    WHERE
+        system_model.name = 'mcs'
+    GROUP BY
+        station, instrument
+)
+
+SELECT
+    ts.station as station,
+    ts.instrument as instrument, 
+    datadays,
+    totaldays,
+    datadays::real / totaldays * 100 as uptime,
+    totalimages, 
+    totalbytes,
+    firstday,
+    lastday
+from 
+    totalsummary as ts
+    join
+        sizesummary as ss on ss.station = ts.station and ss.instrument = ts.instrument
+order by
+    ts.station, ts.instrument
--- support/lib/sql/totaldays.sql
+++ support/lib/sql/totaldays.sql
@@ -1,68 +1,14 @@
-with dailysummary as (
-
-    SELECT
-        station,
-        instrument,
-        count(island) as numimages,
-        min(timestamp) as firsttime,
-        max(timestamp) as lasttime
-    FROM
-        image_islands
-    GROUP BY
-        station, instrument, island
-    ORDER BY
-        station, instrument, island
-
-), totalsummary as (
-
-    select
-        station,
-        instrument,
-        count(numimages) as datadays,
-        sum(numimages) as totalimages,
-        min(firsttime::date) as firstday,
-        max(lasttime::date) as lastday,
-        max(lasttime::date) - min(firsttime::date) + 1 as totaldays 
-    from 
-        dailysummary
-    group by
-        station, instrument
-
-), sizesummary as (
-
-    select
-        station.name as station,
-        instrument.name as instrument,
-        sum(image.image_bytes) as totalbytes
-    from    
-        stationinstrument
-        join
-            station on station.id = stationinstrument.station_id
-        join
-            instrument on instrument.id = stationinstrument.instrument_id
-        join
-            system_model on system_model.id = station.model_id
-        join
-            image on image.stationinstrument_id = stationinstrument.id
-    where
-        system_model.name = 'mcs'
-    group by
-        station, instrument
-)
-
 select
-    ts.station as "Station",
-    ts.instrument as "Instrument",
+    station as "Station",
+    instrument as "Instrument",
     datadays as "Data Days",
     totaldays as "Total Days",
-    format('%s %%', to_char(datadays::real / totaldays * 100, '999')) as "Uptime",
+    format('%s %%', to_char(uptime, '999')) as "Uptime",
     totalimages as "Total Images",
     format('%10s', pg_size_pretty(totalbytes)) as "Total Bytes",
-    to_char(firstday, 'YYYY-MM-DD') as "First Day",
-    to_char(lastday, 'YYYY-MM-DD') as "Last Day"
+    firstday as "First Day",
+    lastday as "Last Day"
 from 
-    totalsummary as ts
-    join
-        sizesummary as ss on ss.station = ts.station and ss.instrument = ts.instrument
+    total_summary 
 order by
-    ts.station, ts.instrument
+    station, instrument
